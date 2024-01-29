44b0ffdf8fdf9dd88e6b7abae61f244a61982691
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-29 15:01:56 +0500

    ...

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { AppContext } from "../context";
+
 const Meals = () => {
+    const context = useContext(AppContext);
+    console.log(context)
     return (
         <h3>Meals Component</h3>
     );
