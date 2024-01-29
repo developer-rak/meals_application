f723e91b759888b3390df2e58c4a5b937a499f3a
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-29 21:57:46 +0500

    ...

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,8 +1,7 @@
-import { useContext } from "react";
-import { AppContext } from "../context";
+import { useGlobalContext } from "../context";
 
 const Meals = () => {
-    const context = useContext(AppContext);
+    const context = useGlobalContext();
     console.log(context)
     return (
         <h3>Meals Component</h3>
--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,14 @@ import React, { useContext } from 'react';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value='hello'>
+
+    return <AppContext.Provider value={{name:'rak', role:'student'}}>
         {children}
     </AppContext.Provider>;
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
 }
  
 export { AppContext, AppProvider };
