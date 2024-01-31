51897ce3adf14c07f205817ae1cb9223a81b56bc
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-01-31 11:22:59 +0500

    useEffect

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+
+    const fetchData = async() => {
+        try {
+            const response = await fetch('https://randomuser.me/api/')
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    },[])
 
     return <AppContext.Provider value={{name:'rak', role:'student'}}>
         {children}
