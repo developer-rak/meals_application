1f0ee00dac26594c9dd52f564d3e3b98a8ddbcd2
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-02-02 14:00:32 +0500

    added loading msg

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,7 +2,13 @@ import { useGlobalContext } from "../context";
 import { BsHandThumbsUp } from "react-icons/bs";
 
 const Meals = () => {
-    const {meals} = useGlobalContext();
+    const {meals, loading} = useGlobalContext();
+
+    if(loading) {
+        return <section className="section">
+            <h4>Loading...</h4>
+        </section>
+    }
     
     return <section className="section-center">
         {meals.map((singleMeal) => {
--- src/context.jsx
+++ src/context.jsx
@@ -7,22 +7,25 @@ const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
     const [meals, setMeals] = useState([]);
 
     const fetchMeals = async(url) => {
+        setLoading(true)
         try {
             const {data} = await axios(url)
             setMeals(data.meals);
         } catch (error) {
             console.log(error.response)
         }
+        setLoading(false)
     }
 
     useEffect(() => {
         fetchMeals(allMealsUrl)
     },[])
 
-    return <AppContext.Provider value={{meals}}>
+    return <AppContext.Provider value={{loading, meals}}>
         {children}
     </AppContext.Provider>;
 }
