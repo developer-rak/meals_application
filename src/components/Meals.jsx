f655c6616ec7e43d13d9463e911a82a400e22876
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-02-03 12:22:35 +0500

    no match item

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -9,6 +9,12 @@ const Meals = () => {
             <h4>Loading...</h4>
         </section>
     }
+
+    if(meals.length < 1) {
+        return <section className="section">
+            <h4>No meals matched your search term. Please try again.</h4>
+        </section>
+    }
     
     return <section className="section-center">
         {meals.map((singleMeal) => {
--- src/context.jsx
+++ src/context.jsx
@@ -14,7 +14,11 @@ const AppProvider = ({ children }) => {
         setLoading(true)
         try {
             const {data} = await axios(url)
-            setMeals(data.meals);
+            if(data.meals) {
+                setMeals(data.meals);
+            } else {
+                setMeals([]);
+            }
         } catch (error) {
             console.log(error.response)
         }
