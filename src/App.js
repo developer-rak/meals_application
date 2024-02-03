8868dee664b0666af4c138013c52d09899e33f24
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2024-02-03 13:24:45 +0500

    added search and buttons also invoke functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import './App.css';
+import Search from './components/Search';
 // import Favorites from './components/Favorites';
 import Meals from './components/Meals';
 // import Modals from './components/Modals';
-// import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <p>Meal Application</p>
-      </header>
-      <main>
-        {/* <Search /> */}
+    <main>
+        <Search />
         {/* <Favorites /> */}
-
         <Meals />
-
         {/* <Modals /> */}
       </main>
-    </div>
   );
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+import { useGlobalContext } from '../context';
+
 const Search = () => {
+    const [text, setText] = useState('');
+
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <h1>Search Component</h1>
+        <header className="search-container">
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={text} placeholder='type favorite meal' className="form-input" />
+                <button type='submit' className="btn">Search</button>
+                <button type='button' className="btn btn-hipster">Surprize me!</button>
+            </form>
+        </header>
     );
 }
  
